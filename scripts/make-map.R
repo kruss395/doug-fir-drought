9ca0c1910c7c7b3caa71a1187c7c7b2d888f54a1
Katie Russell <kruss395@mtroyal.ca>
2022-04-03 13:39:33 -0600

    edits sunday april 3

--- scripts/make-map.R
+++ scripts/make-map.R
@@ -19,7 +19,7 @@ alb_proj = '+proj=aea +lat_1=50 +lat_2=70 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0
 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 
 regions <- readOGR("data/map-data/geographic/ne_50m_admin_1_states_provinces_lakes.shp", 'ne_50m_admin_1_states_provinces_lakes', encoding='UTF-8')
-regions <- subset(regions, geonunit %in% c("United States of America", "Canada"))
+regions <- subset(regions, geonunit %in% c("United States of America", "Canada", "Mexico"))
 regions <- subset(regions, !(name %in% c("Hawaii"))) # region is defined in the first part of the code (see above)
 # region is defined in the first part of the code (see above)
 # regions = spTransform(regions, alb_proj)
@@ -27,36 +27,36 @@ regions = spTransform(regions, CRS("+init=epsg:4326"))
 
 
 # ecoregions for north america
-eco = readOGR('data/map-data/ecoregions/', layer='NA_CEC_Eco_Level2')
+eco = readOGR('data/map-data/ecoregions', layer='NA_CEC_Eco_Level2')
 
 # shapefile reprojection
 # eco_reproj = spTransform(eco, alb_proj)
 eco_reproj = spTransform(eco, CRS("+init=epsg:4326"))
 
 ggplot() + 
-  geom_polygon(data=regions, aes(long,lat, group = group, fill = group)) + 
-  geom_path(data=regions, aes(long,lat, group = group), color="white") +
-  # geom_point(data=dat, aes(x=long, y=lat)) +
+  #geom_polygon(data=regions, aes(long,lat, group = group, fill = "white")) + 
+  geom_path(data=regions, aes(long,lat, group = group), color="black") +
+  geom_point(data=dat, aes(x=long, y=lat), color = "purple") +
   # coord_equal(xlim = c(-123, -108),
   #             ylim = c(48, 60.5)) + 
-  # coord_map(xlim = c(-123, -108),
-  #           ylim = c(48, 60.5)) +
-  # xlim(xlim = c(-123, -108)) +
+  coord_map(xlim = c(-125, -75),
+            ylim = c(10, 60.5)) +
+  xlim(xlim = c(-180, -50)) +
   theme_bw() + 
   coord_equal() +
   guides(fill = FALSE)
 
 
-ggplot() + 
-  geom_polygon(data=regions, aes(long,lat, group = group, fill = group)) + 
-  geom_path(data=regions, aes(long,lat, group = group), color="white") +
-  geom_path(data = eco, aes(x=long, y=lat, group=group)) +
-  # geom_point(data=dat, aes(x=long, y=lat)) +
-  # coord_equal(xlim = c(-123, -108),
-  #             ylim = c(48, 60.5)) + 
-  # coord_map(xlim = c(-123, -108),
-  #           ylim = c(48, 60.5)) +
-  # xlim(xlim = c(-123, -108)) +
-  theme_bw() + 
-  coord_equal() +
-  guides(fill = FALSE)
+# ggplot() + 
+#   geom_polygon(data=regions, aes(long,lat, group = group, fill = group)) + 
+#   geom_path(data=regions, aes(long,lat, group = group), color="white") +
+#   geom_path(data = eco, aes(x=long, y=lat, group=group)) +
+#   # geom_point(data=dat, aes(x=long, y=lat)) +
+#   # coord_equal(xlim = c(-123, -108),
+#   #             ylim = c(48, 60.5)) + 
+#   # coord_map(xlim = c(-123, -108),
+#   #           ylim = c(48, 60.5)) +
+#   # xlim(xlim = c(-123, -108)) +
+#   theme_bw() + 
+#   coord_equal() +
+#   guides(fill = FALSE)
